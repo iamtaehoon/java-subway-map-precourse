39341d52e71407a38b7c9cbef455cfc3575e51fd
iamtaehoon <kimth9981@naver.com>
2021-12-15 18:20:45 +0900

    test(Line): 노선 삭제 테스트 작성

--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
@@ -37,7 +38,7 @@ class LineTest {
     @DisplayName("노선의 이름이 2자리보다 작으면 예외를 만든다.")
     void 노선_이름_글자수_적음_예외() {
         LineService lineService = new LineService();
-        Assertions.assertThatThrownBy(() -> lineService.registerLine("선", "잠실역", "판교역"))
+        assertThatThrownBy(() -> lineService.registerLine("선", "잠실역", "판교역"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -46,7 +47,7 @@ class LineTest {
     void 노선_이름_중복_예외() {
         LineService lineService = new LineService();
         lineService.registerLine("1호선", "잠실역", "판교역");
-        Assertions.assertThatThrownBy(() -> lineService.registerLine("1호선", "나루역", "서울대공원역"))
+        assertThatThrownBy(() -> lineService.registerLine("1호선", "나루역", "서울대공원역"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -55,5 +56,28 @@ class LineTest {
     void 역_존재_안함_예외() {
         LineService lineService = new LineService();
         lineService.registerLine("1호선", "태훈역", "판교역");
+    }
+
+    /**
+     * 노선 삭제
+     */
+
+    @Test
+    @DisplayName("정상적으로 노선을 삭제한다.")
+    void 노선_삭제_정상() {
+        LineService lineService = new LineService();
+        lineService.registerLine("1호선", "잠실역", "판교역");
+        lineService.removeLine("1호선");
+    }
+
+    @Test
+    @DisplayName("삭제하려는 노선이 존재하지 않을 때 예외를 반환한다.")
+    void 존재하지_않는_노선_삭제_예외() {
+        LineService lineService = new LineService();
+        lineService.registerLine("1호선", "잠실역", "판교역");
+        assertThatThrownBy(() -> lineService.removeLine("10호선"))
+            .isInstanceOf(IllegalArgumentException.class);
+
+
     }
 }
